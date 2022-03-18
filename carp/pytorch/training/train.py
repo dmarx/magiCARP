b16985ff7175a7477a986f3c3e6f2bc4f1f06dac
David Marx <david.marx84@gmail.com>
2022-03-18 14:40:59 -0700

    eval step complicated with batchElement.. backing out eval_batch_size changes for now.

--- carp/pytorch/training/train.py
+++ carp/pytorch/training/train.py
@@ -219,24 +219,24 @@ def train(
                 trainer.model.eval()
                 trainer.before_validate_step()
                 eval_batch_size = trainer.train_config.batch_size
-                if hasattr(trainer.train_config, 'eval_batch_size'):
-                    eval_batch_size = trainer.train_config.eval_batch_size
-                eval_batch_size = min(eval_batch_size, trainer.train_config.validation_size)
-                if not trainer.train_config.validation_size % eval_batch_size == 0:
-                    warnings.warn("validation_size is not evenly divisible by eval_batch_size. This may impact correctness of validation metrics.")
+                #if hasattr(trainer.train_config, 'eval_batch_size'):
+                #    eval_batch_size = trainer.train_config.eval_batch_size
+                #eval_batch_size = min(eval_batch_size, trainer.train_config.validation_size)
+                #if not trainer.train_config.validation_size % eval_batch_size == 0:
+                #    warnings.warn("validation_size is not evenly divisible by eval_batch_size. This may impact correctness of validation metrics.")
                 eval_data = trainer.construct_dataloader_eval(evalset, tokenizer, multi_gpus, batch_size=eval_batch_size)
 
-                #eval_out = trainer.eval_step(eval_data)
-                evals = [trainer.eval_step(eval_datum) for eval_datum in eval_data]
-                #n = len(evals)
-                eval_out = {
-                    'Loss/Validation':0, 
-                    'Acc/Validation':0
-                    }
-                for eval in evals:
-                    eval_out['Loss/Validation'] += eval['Loss/Validation']
-                    eval_out['Acc/Validation'] += eval['Acc/Validation'] * eval_batch_size # this won't be strictly accurate if validation_size isn't evenly divisible by eval_batch_size
-                eval_out['Acc/Validation'] = eval_out['Acc/Validation'] / trainer.train_config.validation_size
+                eval_out = trainer.eval_step(eval_data)
+                # I give up. saving this for later.
+                #evals = [trainer.eval_step(eval_datum) for eval_datum in eval_data]
+                #eval_out = {
+                #    'Loss/Validation':0, 
+                #    'Acc/Validation':0
+                #    }
+                #for eval in evals:
+                #    eval_out['Loss/Validation'] += eval['Loss/Validation']
+                #    eval_out['Acc/Validation'] += eval['Acc/Validation'] * eval_batch_size # this won't be strictly accurate if validation_size isn't evenly divisible by eval_batch_size
+                #eval_out['Acc/Validation'] = eval_out['Acc/Validation'] / trainer.train_config.validation_size
 
                 trainer.after_validate_step()
 
